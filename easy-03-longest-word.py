9e922b5f2e192a56ae535f63e8019ba41cff6a85
Serdar Dalgıç <sd@serdardalgic.org>
2014-04-16 14:40:28 +0300

    Add another longest word implementation for easy-03

--- easy-03-longest-word.py
+++ easy-03-longest-word.py
@@ -12,6 +12,11 @@ def LongestWord(sen):
             longest = word if len(word) > len(longest) else longest
     return longest
 
+
+def LongestWord2(sen):
+    return max(filter(lambda x: x.isalpha(), sen.split()), key=len)
+
+
 # keep this function call here
 # to see how to enter arguments in Python scroll down
 print LongestWord(raw_input())
