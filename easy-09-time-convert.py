9d18310f2e0b99586e6eff45f8b30b554361e7fc
Serdar Dalgıç <sd@serdardalgic.org>
2014-04-16 15:24:50 +0300

    Time convert challenge

---
+++ easy-09-time-convert.py
@@ -0,0 +1,13 @@
+# Have the function TimeConvert(num) take the num parameter being passed and
+# return the number of hours and minutes the parameter converts to (ie.
+# if num = 63 then the output should be 1:3). Separate the number of hours
+# and minutes with a colon.
+
+
+def TimeConvert(num):
+    return "{hour}:{minute}".format(hour=num / 60, minute=num % 60)
+
+
+# keep this function call here
+# to see how to enter arguments in Python scroll down
+print TimeConvert(raw_input())
