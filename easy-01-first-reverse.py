84f471817c275f88981abac4b62483da684c0d67
Serdar Dalgıç <sd@serdardalgic.org>
2014-06-20 11:31:27 +0300

    proper name usage.

--- easy-01-first-reverse.py
+++ easy-01-first-reverse.py
@@ -1,21 +1,21 @@
-# Question: Have the function FirstReverse(str) take the str parameter
+# Question: Have the function FirstReverse(str_) take the str parameter
 # being passed and return the string in reversed order.
 
 
-def FirstReverse(str):
+def FirstReverse(str_):
   # code goes here
     rev = ""
-    for x in reversed(str):
+    for x in reversed(str_):
         rev += x
     return rev
 
 
-def FirstReverse2(astr):
-    return astr[::-1]
+def FirstReverse2(str_):
+    return str_[::-1]
 
 
-def FirstReverse3(astr):
-    return "".join([x for x in reversed(astr)])
+def FirstReverse3(str_):
+    return "".join([x for x in reversed(str_)])
 
 
 # keep this function call here
